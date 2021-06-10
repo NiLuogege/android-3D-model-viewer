f48dc11070212681a9702286bcb750cec1a71052
luoChen <hank.luo@xianghuanji.com>
2021-06-10 19:02:12 +0800

    。。

--- app/src/main/java/org/andresoviedo/app/model3D/dewu/DewuActivity.java
+++ app/src/main/java/org/andresoviedo/app/model3D/dewu/DewuActivity.java
@@ -1,78 +1,187 @@
 package org.andresoviedo.app.model3D.dewu;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 import org.andresoviedo.android_3d_model_engine.camera.CameraController;
 import org.andresoviedo.android_3d_model_engine.collision.CollisionController;
 import org.andresoviedo.android_3d_model_engine.controller.TouchController;
+import org.andresoviedo.android_3d_model_engine.services.LoaderTask;
 import org.andresoviedo.android_3d_model_engine.services.SceneLoader;
 import org.andresoviedo.android_3d_model_engine.view.ModelRenderer;
 import org.andresoviedo.android_3d_model_engine.view.ModelSurfaceView;
+import org.andresoviedo.app.model3D.demo.DemoLoaderTask;
 import org.andresoviedo.app.model3D.view.ModelViewerGUI;
+import org.andresoviedo.dddmodel2.R;
+import org.andresoviedo.util.android.ContentUtils;
 import org.andresoviedo.util.event.EventListener;
 
+import java.io.IOException;
 import java.net.URI;
 import java.util.EventObject;
 
 public class DewuActivity extends Activity implements EventListener {
 
-    private int TYPE_FILE_OBJ = 0;
+    private static final int REQUEST_CODE_LOAD_TEXTURE = 1000;
+    private static final int FULLSCREEN_DELAY = 10000;
+
+    /**
+     * Type of model if file name has no extension (provided though content provider)
+     * <p>
+     * 文件类型  0 = obj, 1 = stl, 2 = dae
+     */
+    private int paramType;
+    /**
+     * The file to load. Passed as input parameter
+     */
+    private URI paramUri;
+    /**
+     * Enter into Android Immersive mode so the renderer is full screen or not
+     * 是否全屏模式
+     */
+    private boolean immersiveMode;
+    /**
+     * Background GL clear color. Default is light gray
+     * 设置背景色 RGBA
+     */
     private float[] backgroundColor = new float[]{0.0f, 0.0f, 0.0f, 1.0f};
 
     private ModelSurfaceView gLView;
-    private URI paramUri;
     private TouchController touchController;
+    private SceneLoader scene;
     private ModelViewerGUI gui;
     private CollisionController collisionController;
+
+
+    private Handler handler;
     private CameraController cameraController;
 
-    private void log(String content) {
-        Log.e("DewuActivity", content);
-    }
-
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i("ModelActivity", "Loading activity...");
         super.onCreate(savedInstanceState);
+
+        // Try to get input parameters
         Bundle b = getIntent().getExtras();
-        this.paramUri = URI.create(b.getString("uri"));
+        if (b != null) {
+            try {
+                if (b.getString("uri") != null) {
+                    this.paramUri = new URI(b.getString("uri"));
+                    Log.i("ModelActivity", "Params: uri '" + paramUri + "'");
+                }
+                this.paramType = b.getString("type") != null ? Integer.parseInt(b.getString("type")) : -1;
+                this.immersiveMode = "true".equalsIgnoreCase(b.getString("immersiveMode"));
 
-        log(paramUri.toString());
+                if (b.getString("backgroundColor") != null) {
+                    String[] backgroundColors = b.getString("backgroundColor").split(" ");
+                    backgroundColor[0] = Float.parseFloat(backgroundColors[0]);
+                    backgroundColor[1] = Float.parseFloat(backgroundColors[1]);
+                    backgroundColor[2] = Float.parseFloat(backgroundColors[2]);
+                    backgroundColor[3] = Float.parseFloat(backgroundColors[3]);
+                }
+            } catch (Exception ex) {
+                Log.e("ModelActivity", "Error parsing activity parameters: " + ex.getMessage(), ex);
+            }
+
+        }
+
+        handler = new Handler(getMainLooper());
+
+        // Create our 3D scenario
+        Log.i("ModelActivity", "Loading Scene...");
+        scene = new SceneLoader(this, paramUri, paramType, gLView);
+
+        //当 传进入文件 为空是，开启loader 去加载 文件
+        if (paramUri == null) {
+            final LoaderTask task = new DemoLoaderTask(this, null, scene);
+            task.execute();
+        }
+
+/*        Log.i("ModelActivity","Loading Scene...");
+        if (paramUri == null) {
+            scene = new ExampleSceneLoader(this);
+        } else {
+            scene = new SceneLoader(this, paramUri, paramType, gLView);
+        }*/
+
+        try {
+            Log.i("ModelActivity", "Loading GLSurfaceView...");
+            gLView = new ModelSurfaceView(this, backgroundColor, this.scene);
+            gLView.addListener(this);
+
+            //将 GLSurfaceView 进行显示
+            setContentView(gLView);
+            scene.setView(gLView);
+        } catch (Exception e) {
+            Log.e("ModelActivity", e.getMessage(), e);
+            Toast.makeText(this, "Error loading OpenGL view:\n" + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        //创建触摸控制器
+        try {
+            Log.i("ModelActivity", "Loading TouchController...");
+            touchController = new TouchController(this);
+            touchController.addListener(this);
+        } catch (Exception e) {
+            Log.e("ModelActivity", e.getMessage(), e);
+            Toast.makeText(this, "Error loading TouchController:\n" + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        //创建碰撞控制器
+        try {
+            Log.i("ModelActivity", "Loading CollisionController...");
+            collisionController = new CollisionController(gLView, scene);
+            collisionController.addListener(scene);
+            touchController.addListener(collisionController);
+            touchController.addListener(scene);
+        } catch (Exception e) {
+            Log.e("ModelActivity", e.getMessage(), e);
+            Toast.makeText(this, "Error loading CollisionController\n" + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        //创建相机控制器
+        try {
+            Log.i("ModelActivity", "Loading CameraController...");
+            cameraController = new CameraController(scene.getCamera());
+            gLView.getModelRenderer().addListener(cameraController);
+            touchController.addListener(cameraController);
+        } catch (Exception e) {
+            Log.e("ModelActivity", e.getMessage(), e);
+            Toast.makeText(this, "Error loading CameraController" + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        try {
+            // TODO: finish UI implementation
+            Log.i("ModelActivity", "Loading GUI...");
+            gui = new ModelViewerGUI(gLView, scene);
+            touchController.addListener(gui);
+            gLView.addListener(gui);
+            scene.addGUIObject(gui);
+        } catch (Exception e) {
+            Log.e("ModelActivity", e.getMessage(), e);
+            Toast.makeText(this, "Error loading GUI" + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
 
 
-        SceneLoader scene = new SceneLoader(this, paramUri, TYPE_FILE_OBJ, gLView);
-        gLView = new ModelSurfaceView(this, backgroundColor, scene);
-
-        setContentView(gLView);
-        scene.setView(gLView);
-//
-//
-////创建触摸控制器
-//        touchController = new TouchController(this);
-//        touchController.addListener(this);
-//
-////创建碰撞控制器
-//        collisionController = new CollisionController(gLView, scene);
-//        collisionController.addListener(scene);
-//        touchController.addListener(collisionController);
-//        touchController.addListener(scene);
-//
-////创建相机控制器
-//        cameraController = new CameraController(scene.getCamera());
-//        gLView.getModelRenderer().addListener(cameraController);
-//        touchController.addListener(cameraController);
-//
-//        gui = new ModelViewerGUI(gLView, scene);
-//        touchController.addListener(gui);
-//        gLView.addListener(gui);
-//        scene.addGUIObject(gui);
-
-
+        // 加载mode
+        // load model
         scene.init();
     }
+
+
 
     @Override
     public boolean onEvent(EventObject event) {
